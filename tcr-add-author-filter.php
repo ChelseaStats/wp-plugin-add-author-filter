d739cc8a0c8ead8fd112307e8e165f1688a290dd
ChelseaStats <git@thechels.co.uk>
2015-07-05 02:49:37 +0100

    Some more info added

--- tcr-add-author-filter.php
+++ tcr-add-author-filter.php
@@ -1,36 +1,36 @@
 <?php
-/*
-Plugin Name: TCR Add Author Filter
-Description: Author filter for WordPress admin.
-Version: 1.0.0
-Plugin URI: http://thecellarroom.uk
-Author: The Cellar Room Limited
-Author URI: http://www.thecellarroom.uk
-Copyright (c) 2015 The Cellar Room Limited
-*/
+	/*
+	Plugin Name: TCR Add Author Filter
+	Description: Author filter for WordPress admin.
+	Version: 1.0.0
+	Plugin URI: http://thecellarroom.uk
+	Author: The Cellar Room Limited
+	Author URI: http://www.thecellarroom.uk
+	Copyright (c) 2015 The Cellar Room Limited
+	*/
 
-defined( 'ABSPATH' ) or die();
+	defined( 'ABSPATH' ) or die();
 
-/*************************************************************************/
+	/*************************************************************************/
 
-if ( ! class_exists( 'tcr_author_filter' ) ) :
+	if ( ! class_exists( 'tcr_author_filter' ) ) :
 
-		class tcr_author_filter {
+			class tcr_author_filter {
 
-			function __construct() {
-				add_action('restrict_manage_posts', array( $this, 'author_filter'));
-			}
-		
-			function author_filter() { 
-			$args = array('name' => 'author', 'show_option_all' => 'View all authors'); 
-				 if (isset($_GET['user'])) { 
-						   $args['selected'] = $_GET['user']; 
+				function __construct() {
+					add_action('restrict_manage_posts', array( $this, 'author_filter'));
 				}
-			wp_dropdown_users($args); 
+
+				function author_filter() {
+				$args = array('name' => 'author', 'show_option_all' => 'View all authors');
+					 if (isset($_GET['user'])) {
+							   $args['selected'] = $_GET['user'];
+					}
+				wp_dropdown_users($args);
+				}
+
+
 			}
-			
+			new tcr_author_filter;
 
-		}
-		new tcr_author_filter;
-
-endif;
+	endif;
